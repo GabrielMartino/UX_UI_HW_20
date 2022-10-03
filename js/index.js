fc345eba5cef2006b4fc89f277e0113b264e636d
Gabriel Martino <GabrielMartino@users.noreply.github.com>
2022-10-03 22:10:20 +1000

    homework 19 submit

--- js/index.js
+++ js/index.js
@@ -1,8 +1,4 @@
-/*
-$("button").click(function() {
-    $(this).toggleClass('buttonClicked');
-  });
-*/
+
   $(function() {
     $("button")
       .on("click", function(){
@@ -12,4 +8,37 @@ $("button").click(function() {
       $(this).removeClass('buttonClicked');
     });
 	});
+
+ 
+
+  VanillaTilt.init(document.querySelectorAll(".tiltElement1"), {
+    max:20,
+    speed:800,
+  
+  })
+
+  VanillaTilt.init(document.querySelectorAll(".tiltElement2"), {
+    max:20,
+    speed:800,
+    scale:1.2
+  })
+
+  $(document).ready(function()
+  {
+      $("#cloud1, #cloud2, #cloud3").css("transition", "transform 400ms ease-in-out");
+  
+      $("#cloud1, #cloud2, #cloud3").hover(    
+          // Handler for mouseenter
+          function()
+          {
+              $(this).css("transform", "scale(1.2)");
+              $(this).css("color", "red");
+          },
+          // Handler for mouseleave
+          function()
+          {
+              $(this).css("transform", "scale(1)");
+          }
+      );
+  });
 
